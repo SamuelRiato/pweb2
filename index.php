22e3175606b4567c7070871483fb6a7348bb8096
SamuelRiato <riatos01@gmail.com>
2026-03-03 10:57:11 -0300

    Costrução da estrutura PHP e inicio da criação dos atributos

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
+<?php
+/*declaração de atributos(mesma coisa de variavel)*/
+$imagem = 'midias/2321306_625.jpg';
+$titulo = 'Placa de vídeo RTX5090';
+$resumo = 'Placa gráfica top de linha feita para jogos';
+$valor = 19999.99;
+
+$imagem2 = 'midias/8bf93226233843836eb665242e6cd0b4.webp';
+$titulo2 = 'Gabinete aquario';
+$resumo2 = 'Gabinete grande com muito espaço para por as peças do seu computador';
+$valor2 = 500.00;
+
+$imagem3 = 'midias/aorus.webp';
+$titulo3 = 'Placa de vídeo RTX5090';
+$resumo3 = 'Placa gráfica top de linha feita para jogos';
+$valor3 = 19999.99;
+
+$imagem4 = 'midias/qual_memoria_ram_comprar_corpo.webp';
+$titulo4 = 'Memoria RAM 8GB';
+$resumo4 = 'Placa gráfica top de linha feita para jogos';
+$valor4 = 1999.99;
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <title>Lojinha</title>
 </head>
 <body>
@@ -41,48 +63,52 @@
         <div class="row mt-2">
             <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="midias/2321306_625.jpeg" class="card-img-top img-fluid" alt="...">
+                    <img src="<?php echo $imagem ?>" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Produto 1</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <h5 class="card-title"><?php echo $titulo ?></h5>
+                      <p class="card-text"><?php echo $resumo ?></p>
+                    <p class="text-danger">$<?php echo $valor ?></p>
+                      <a href="#" class="btn btn-outline-dark">Mostrar produto</a>
                     </div>
                   </div>
             </div>
             <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="midias/8bf93226233843836eb665242e6cd0b4.webp" class="card-img-top img-fluid" alt="...">
+                <img src="<?php echo $imagem2 ?>" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Produto 2</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <h5 class="card-title"><?php echo $titulo2 ?></h5>
+                      <p class="card-text"><?php echo $resumo2 ?></p>
+                    <p class="text-danger">$<?php echo $valor2 ?></p>
+                      <a href="#" class="btn btn-outline-dark">Mostrar produto</a>
                     </div>
                   </div>
             </div>
             <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="midias/aorus.webp" class="card-img-top img-fluid" alt="...">
+                <img src="<?php echo $imagem3 ?>" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Produto 3</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <h5 class="card-title"><?php echo $titulo3 ?></h5>
+                      <p class="card-text"><?php echo $resumo3 ?></p>
+                    <p class="text-danger">$<?php echo $valor3 ?></p>
+                      <a href="#" class="btn btn-outline-dark">Mostrar produto</a>
                     </div>
                   </div>
             </div>
             <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="midias/qual_memoria_ram_comprar_corpo.webp" class="card-img-top img-fluid" alt="...">
+                <img src="<?php echo $imagem4 ?>" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Produto 4</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <h5 class="card-title"><?php echo $titulo4 ?></h5>
+                      <p class="card-text"><?php echo $resumo4 ?></p>
+                    <p class="text-danger">$<?php echo $valor4 ?></p>
+                      <a href="#" class="btn btn-outline-dark">Mostrar produto</a>
                     </div>
                   </div>
             </div>
         </div>
     </div>
 </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 
 
 </body>
